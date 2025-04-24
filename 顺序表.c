d8e790c202d848f4921f2da69d8ee9f7d7a33b95
HuhGee YEEE! <2453374@tongji.edu.cn>
2025-04-24 14:50:12 +0800

    Add files via upload

--- 顺序表.c
+++ 顺序表.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #define MAXSIZE 100
-typedef int elemType;
 
+typedef int elemType;
 //定义顺序表结构体 
 typedef struct
 {
-	int data[MAXSIZE];
+	elemType * data;
 	int length;
 }SQL;
 
-//初始化顺序表
-void initList(SQL * p)
+//更强大的初始化顺序表(堆内存开辟) 
+SQL *initList()
 {
-	p->length = 0;
-}
+	SQL *L = (SQL *)malloc(sizeof(SQL));
+	L->data = (elemType *)malloc(sizeof(elemType) * MAXSIZE);
+	L->length = 0;
+	return L;
+} 
 
 //遍历顺序表
 void showElem(SQL * L)
 {
-	if(L->length == 0)
+	if(getLength(L) == 0)
 	{
 		printf("当前顺序表为空");
 		return;
 	}
 	printf("顺序表长度为%d，元素: ", L->length);
-	for(int i = 0; i < L->length; i++)
+	for(int i = 0; i < getLength(L); i++)
 	{
 		printf("%d ", L->data[i]);
 	}
@@ -103,37 +106,57 @@ int findElem(SQL * L, elemType e)
 	return index;
 }
 
+//获得线性表长度
+int getLength(SQL *L)
+{
+ 	return  L->length;	
+} 
+
+//清空线性表
+void clearList(SQL *L)
+{
+	L->length = 0;
+	printf("清空顺序表成功！\n");
+} 
+
 //测试代码 
 int main(int args, const char argv[])
 {
 	//初始化线性表 
-	SQL sList;
-	initList(&sList);
+	SQL *sList = initList();
 	printf("初始化顺序表成功!\n占用内存为%d字节\n", sizeof(sList));
 	
 	//尾插元素1 2 3 4 
-	appendElem(&sList, 1);
-	appendElem(&sList, 2);
-	appendElem(&sList, 3);
-	appendElem(&sList, 4);
-	showElem(&sList);
+	appendElem(sList, 1);
+	appendElem(sList, 2);
+	appendElem(sList, 3);
+	appendElem(sList, 4);
+	showElem(sList);
 	
 	//在0处插入0 
-	insertElem(&sList, 0, 0);
+	insertElem(sList, 0, 0);
 	printf("在0处插入0后结果为");
-	showElem(&sList);
+	showElem(sList);
 	
 	//删除0处元素0
 	int a;
-	deleteElem(&sList, 0, &a);
+	deleteElem(sList, 0, &a);
 	printf("删除0处的0结果为");
-	showElem(&sList);
+	showElem(sList);
 	
 	//查找1 2 3 4 0
-	printf("找到元素索引为%d\n", findElem(&sList, 1));
-	printf("找到元素索引为%d\n", findElem(&sList, 2));
-	printf("找到元素索引为%d\n", findElem(&sList, 3));
-	printf("找到元素索引为%d\n", findElem(&sList, 4));
-	printf("找到元素索引为%d\n", findElem(&sList, 0));
+	printf("找到元素索引为%d\n", findElem(sList, 1));
+	printf("找到元素索引为%d\n", findElem(sList, 2));
+	printf("找到元素索引为%d\n", findElem(sList, 3));
+	printf("找到元素索引为%d\n", findElem(sList, 4));
+	printf("找到元素索引为%d\n", findElem(sList, 0));
+	
+	//清空顺序表
+	clearList(sList);
+	showElem(sList); 
+	
+	//返回长度 
+	free(sList->data);
+	free(sList);
 	return 0;
 }
